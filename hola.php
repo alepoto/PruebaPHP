546590261de8362fc5ba343b271048fb07b189c7
Diego Sanchez <alepoto89@hotmail.com>
2018-02-27 11:16:37 -0500

    nuevo archivo

---
+++ hola.php
@@ -0,0 +1,5 @@
+<?php
+/*imprimir hola mundo*/
+echo "Hola Mundo"; 
+
+?>
---
+++ prueba.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Ejemplo de Uso For</title>
+</head>
+
+<body>
+
+Ejemplo de un ciclo
+<BR></BR>
+
+<?php
+	echo "codigo PHP con uso FOR <br>";
+
+	for($i=2;$i<10;$i=$i+2)
+	{
+		echo "Linea " .$i. "<br>";
+	}
+	?>
+
+	</body>
+	</htmml>
